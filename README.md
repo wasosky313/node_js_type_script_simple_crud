2df3503adfe6d0dfe703f54d6a6404b053a74bf0
Andy Jimenez Reyes <wasosky313@gmail.com>
2024-07-07 12:38:15 -0300

    add CRUD

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # node_js_type_script_simple_crud
+
+## docs
+https://www.bezkoder.com/node-js-typescript-mysql/
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { Application } from "express";
+import cors, { CorsOptions } from "cors";
+import Routes from './routes';
+
+export default class Server {
+  constructor(app: Application) {
+    this.config(app);
+    new Routes(app);
+  }
+
+  private config(app: Application): void {
+    const corsOptions: CorsOptions = {
+      origin: "*"
+    };
+
+    app.use(cors(corsOptions));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+  }
+}
